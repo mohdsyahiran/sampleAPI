7d85257f7c421d107ddce7eff1cf5cec308903fa
mohdsyahiran <mohamad.syahiran25@gmail.com>
2024-06-24 14:42:25 +0800

    Improve more in API function

--- api.php
+++ api.php
@@ -2,19 +2,19 @@
 header("Content-Type: application/json");
 
 // Check if a specific query parameter is set
-if (isset($_GET['response'])) {
-    $response = $_GET['response'];
-    // Validate the response to ensure it is either "y" or "n"
-    if ($response === 'yes') {
+if (isset($_GET['flag'])) {
+    $flag = $_GET['flag'];
+    // Validate the flag to ensure it is either "y" or "n"
+    if ($flag === 'true') {
         $message = 'Your account has been reactivated.';
-    } elseif ($response === 'no') {
+    } elseif ($flag === 'false') {
         $message = 'Your account failed to reactivate.';
     } else {
-        $message = 'Invalid response. Please use "true" or "false".';
+        $message = 'Invalid flag. Please use "true" or "false".';
     }
 } else {
     // Default response when the query parameter is not set
-    $message = 'Please provide a "response" query parameter with value "yes" or "no".';
+    $message = 'Please provide a "flag" query parameter with value "true" or "false".';
 }
 
 // Output the response as JSON
