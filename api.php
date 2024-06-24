3b259434a70b503e21cd46204aaee1a9510637ea
mohdsyahiran <mohamad.syahiran25@gmail.com>
2024-06-24 14:35:40 +0800

    Improve response

--- api.php
+++ api.php
@@ -1,9 +1,22 @@
 <?php
 header("Content-Type: application/json");
 
-// Randomly return "yes" or "no"
-$response = (rand(0, 1) === 1) ? "yes" : "no";
+// Check if a specific query parameter is set
+if (isset($_GET['response'])) {
+    $response = $_GET['response'];
+    // Validate the response to ensure it is either "y" or "n"
+    if ($response === 'yes') {
+        $message = 'Your account has been reactivated.';
+    } elseif ($response === 'no') {
+        $message = 'Your account failed to reactivate.';
+    } else {
+        $message = 'Invalid response. Please use "true" or "false".';
+    }
+} else {
+    // Default response when the query parameter is not set
+    $message = 'Please provide a "response" query parameter with value "yes" or "no".';
+}
 
 // Output the response as JSON
-echo json_encode(["response" => $response]);
+echo json_encode(["message" => $message]);
 ?>
